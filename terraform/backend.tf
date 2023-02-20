2716bd5254c86bb38d8032a76c2841574861d1c4
Antoine Kevin CHOULA <76167680+kevAnto@users.noreply.github.com>
2023-02-20 16:36:56 +0000

    update remote backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
+    bucket = "remote-state-app"
     region = "us-east-1"
-    key = "eks/terraform.tfstate"
+    key    = "eks/terraform.tfstate"
   }
 }
