d37b865f415cf1b7b1ab9c5a97ef2f9cee90add1
Antoine CHOULA <76167680+kevAnto@users.noreply.github.com>
2023-08-16 10:40:02 +0100

    Update vpc version

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,7 @@
 data "aws_availability_zones" "azs" {}
 module "myapp-vpc" {
   source          = "terraform-aws-modules/vpc/aws"
-  version         = "3.19.0"
+  version         = "5.1.0"
   name            = "myapp-vpc"
   cidr            = var.vpc_cidr_block
   private_subnets = var.private_subnet_cidr_blocks
